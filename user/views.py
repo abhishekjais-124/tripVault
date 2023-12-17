af01282ab970ee31bde00a081c63651201eb89e7
aj <abhishekjaiswal124@gmail.com>
2023-12-18 02:28:54 +0530

    feat: user registration

--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views import View
+from django.contrib import messages
+
+from .forms import CustomerRegistrationForm
 
 # Create your views here.
+class CustomerRegistrationView(View):
+    def get(self, request):
+        form = CustomerRegistrationForm()
+        return render(request, 'user/customerregistration.html', {'form': form})
+
+    def post(self, request):
+        form = CustomerRegistrationForm(request.POST)
+        if form.is_valid():
+            messages.success(
+                request, 'Congratulations!! Registered Successfully')
+            form.save()
+        return render(request, 'user/customerregistration.html', {'form': form})
+
