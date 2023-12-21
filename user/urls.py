77f372e64cc70f9c2b2b12edf1d3fafb7ea9e3ba
aj <abhishekjaiswal124@gmail.com>
2023-12-22 01:30:09 +0530

    Adding templates for groups and creating models

--- user/urls.py
+++ user/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html',authentication_form=LoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('profile/', views.UserProfile.as_view(), name='userprofile'),  # for the registration
+    path('groups/', views.GroupView.as_view(), name='group'),
 ]
